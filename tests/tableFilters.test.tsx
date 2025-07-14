3ad5fca409b0c1a3b30dd15e3770189bc04cf101
ensiyehe <ensiyeh.esfandiari@gmail.com>
2025-07-14 15:26:20 +0200

    edittextfilter action creator function redux tested -without the request (Api)

---
+++ tests/tableFilters.test.tsx
@@ -0,0 +1,48 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithProviders from './tableFiltersSetupStore';
+import TableFilters from '../src/components/shared/TableFilters';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const mockLoadResource = createAsyncThunk('resource/load', async () => Promise.resolve());
+const mockLoadResourceIntoTable = () => (dispatch:any) => {}; // dummy thunk does nothing
+const mockResource = "testResource";
+
+describe('TableFilterText', () => {
+  test('dispatches editTextFilter action when input changes', async () => {
+    const user = userEvent.setup();
+
+    // initial preloaded state with empty textFilter
+    const preloadedState = {
+      tableFilters: {
+        data: [],
+        stats: {},
+        textFilter: "",
+        selectedFilter: null,
+        secondFilter: null,
+        currentResource: mockResource,
+      },
+    tableFilterProfiles: {
+    profiles: [],
+  },
+    };
+
+    const { store } = renderWithProviders(
+      <TableFilters
+        loadResource={mockLoadResource}
+        loadResourceIntoTable={mockLoadResourceIntoTable}
+        resource={mockResource}
+      />,
+      { preloadedState }
+    );
+
+    const input = screen.getByPlaceholderText(/Search/i);
+
+    // Simulate typing "hello"
+    await user.clear(input);
+    await user.type(input, 'hello');
+
+    // Check Redux store textFilter value updated
+    expect(store.getState().tableFilters.textFilter).toBe('hello');
+  });
+});
---
+++ tests/tableFiltersSetupStore.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode, ReactElement } from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { configureStore, EnhancedStore } from "@reduxjs/toolkit";
+import { RenderOptions } from "@testing-library/react";
+
+import tableFilterReducer from "../src/slices/tableFilterSlice";
+import tableFilterProfilesReducer from "../src/slices/tableFilterProfilesSlice"
+
+// Your root reducer combining actual reducers
+export const rootReducer = {
+  tableFilters: tableFilterReducer,
+  tableFilterProfiles: tableFilterProfilesReducer,
+};
+
+
+export function createTestStore(preloadedState = {}) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+
+function renderWithProviders(
+  ui: ReactElement,
+  {
+    storeInstance,
+    preloadedState,
+    ...renderOptions
+  }: {
+    storeInstance?: EnhancedStore;
+    preloadedState?: any;
+  } & RenderOptions = {}
+) {
+
+  const store = storeInstance ?? createTestStore(preloadedState);
+
+  function Wrapper({ children }: { children: ReactNode }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
+
+export default renderWithProviders;
