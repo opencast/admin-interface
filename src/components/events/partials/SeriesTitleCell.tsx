401c1032f47d80b3268a77507bde6e4f11d6f958
ensiyehe <ensiyeh.esfandiari@gmail.com>
2025-10-22 14:24:52 +0200

    fix merge conflict

--- src/components/events/partials/SeriesTitleCell.tsx
+++ src/components/events/partials/SeriesTitleCell.tsx
@@ -1,6 +1,5 @@
 import { Series } from "../../../slices/seriesSlice";
 import RedirectCell from "../../shared/RedirectCell";
-import { resetFilterValues } from "../../../slices/tableFilterSlice";
 
 /**
  * This component renders the title cells of series in the table view
@@ -10,22 +9,13 @@ const SeriesTitleCell = ({
 }: {
 	row: Series
 }) => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
-
-	const redirectToEvents = async (seriesId: string) => {
-		dispatch(resetFilterValues());
-		// set the series filter value of events to series title
-		await dispatch(setSpecificEventFilter({ filter: "series", filterValue: seriesId }));
-		navigate("/events/events");
-	};
-
 	return (
 		<RedirectCell
 			path={"/events/events"}
 			filterName={"series"}
 			filterValue={row.id}
 			// tooltipText={"EVENTS.SERIES.TABLE.TOOLTIP.SERIES"} // Disabled due to performance concerns
+			 resetBeforeRedirect
 		>
 			{row.title}
 		</RedirectCell>
