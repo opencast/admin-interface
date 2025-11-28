db160e4ffc7143741b01c5815c60564e48998da6
ensiyehe <ensiyeh.esfandiari@gmail.com>
2025-11-28 08:32:16 +0100

    remove the esc fpcus trap fix

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -69,7 +69,7 @@ export const Modal = forwardRef<ModalHandle, PropsWithChildren<ModalProps>>(
 
 	return ReactDOM.createPortal(
 		isOpen &&
-			<FocusTrap focusTrapOptions={{ escapeDeactivates: false }}>
+			<FocusTrap>
 				<div>
 					<div className="modal-animation modal-overlay" />
 					<section
--- src/components/shared/modals/ModalContentTable.tsx
+++ src/components/shared/modals/ModalContentTable.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import ModalContent from "./ModalContent";
 
 /**
@@ -15,25 +14,13 @@ const ModalContentTable = ({
 	modalBodyChildren?: React.ReactNode
 	children: React.ReactNode
 }) => {
-
-	 // Store the last blurred/focused element
-  	const lastFocusedRef = useRef<HTMLElement | null>(null);
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    const active = document.activeElement as HTMLElement | null;
-      if (event.key === "Escape") {
-        if (active && event.currentTarget.contains(active)) {
-          lastFocusedRef.current = active; // store the last focused element
-          active.blur();
-        }
-      }
-    };
   return (
 		<ModalContent
 			modalContentChildren={modalContentChildren}
 			modalContentClassName={modalContentClassName}
 		>
 			{modalBodyChildren}
-			<div className="full-col" onKeyDown={handleKeyDown}>
+			<div className="full-col">
 				{children}
 			</div>
 		</ModalContent>
